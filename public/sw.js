1e110d7cf7a707872095e9632e0ff26541f81734
monupatel123 <jaao73749@gmail.com>
2026-09-16 14:43:12 +0530

    Update sw.js with Adsterra Multitag service worker

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11814560
+    "domain": "5gvci.com",
+    "zoneId": 11814614
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
