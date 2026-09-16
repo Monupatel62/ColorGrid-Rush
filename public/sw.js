96e15f295a304e8ddddfe6c356e48f1f870263af
monupatel123 <jaao73749@gmail.com>
2026-09-16 21:51:51 +0530

    Update sw.js with Netlify Multitag zone ID 11817681

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11814614
+    "zoneId": 11817681
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
